68dd26adb4e286eb2d3160ca7bb65758a3d1c87d
divyanshusahu <dsahu1997@gmail.com>
2023-02-08 11:40:33 +0530

    command error handling

--- .github/d2m.sh
+++ .github/d2m.sh
@@ -2,3 +2,8 @@
 
 echo "Creating PR";
 gh pr create --base dev --head testing --title testing --body testing;
+if [ $? -eq 0 ]; then
+    echo "PR successfully created"
+else
+    echo "Error in creating PR"
+fi
